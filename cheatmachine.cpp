19b3e037addca35f5313259a0ce12365848eecc6
George Abraham <37941352+georgeabr@users.noreply.github.com>
2026-03-05 16:50:47 +0000

    Add copyright notice and licensing information

--- cheatmachine.cpp
+++ cheatmachine.cpp
@@ -3,6 +3,20 @@
  * Build:  g++ -O3 -std=c++17 -o cheatmachine cheatmachine.cpp -lncurses -lpthread
  * Run:    sudo ./cheatmachine [pid]
  */
+
+/*
+ * Copyright (C) 2026 George Abraham
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ */
+
 #include <cmath>
 #include <algorithm>
 #include <atomic>
